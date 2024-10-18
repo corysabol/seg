ffcfa0760aea4d3a718af391be070f194972c1dc
Cory Sabol <corygrandhamsabol@gmail.com>
2024-10-18 12:07:25 -0700

    implementing custom scanner using tokio

--- src/consts.rs
+++ src/consts.rs
@@ -1,1 +1,3 @@
 pub const NFT_RULES_TEMPLATE: &str = include_str!("rules.nft");
+pub const MAX_TASKS: u8 = 10;
+pub const MAX_SOCKETS: u8 = 1024; // A sensible limit based on typical default OS constraints
--- src/network.rs
+++ src/network.rs
@@ -1,21 +1,67 @@
+use crate::consts::*;
 use crate::firewall::*;
 use crate::util::*;
 
+use socket2::{Domain, Protocol, Socket, Type};
 use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
 use std::process::Stdio;
 use std::sync::Arc;
 use std::vec;
 use tokio::fs::OpenOptions;
 use tokio::io::{AsyncBufReadExt, BufReader, BufWriter};
+use tokio::net::TcpStream;
 use tokio::net::{TcpListener, UdpSocket};
 use tokio::process::Command;
 use tokio::signal::ctrl_c;
+use tokio::sync::Semaphore;
+use tokio::task;
 
 #[derive(Clone, clap::ValueEnum)]
-pub enum Protocol {
+pub enum ScanProtocol {
     TCP,
     UDP,
     BOTH,
+}
+
+pub enum ScanType {
+    TCP_SYN,
+    TCP_FULL,
+    TCP_ACK,
+    ICMP,
+}
+
+enum ScannerSocketIpv4 {
+    TCPSocket(TcpStream),
+    UDPSocket(UdpSocket),
+}
+
+struct Scanner {
+    host: Ipv4Addr,
+    task_limit: u8,
+    upper: u16,
+    lower: u16,
+}
+
+impl Scanner {
+    fn new(host: Ipv4Addr, task_limit: u8, upper: u16, lower: u16) -> Self {
+        Self {
+            host,
+            task_limit,
+            upper,
+            lower,
+        }
+    }
+
+    async fn scan_ports(&self) {
+        // Bucket the ports into tasks based on the concurrency_limit
+        let port_count = self.upper - self.lower;
+        let bucket_size = port_count / self.task_limit as u16;
+        // let buckets = vec![];
+
+        for t in 0..self.task_limit {}
+    }
 }
 
 pub async fn run_scan(input_file: String, scan_type: Protocol) {
@@ -45,17 +91,24 @@ pub async fn run_scan(input_file: String, scan_type: Protocol) {
     }
 }
 
-pub async fn scan_nmap(listener_ip: &str, output_file: &str, scan_type: Protocol) {
+pub async fn scan(input_file: String, scan_type: Protocol) {
+    let file = tokio::fs::File::open(&input_file)
+        .await
+        .expect("Unable to open input file");
+    let reader = tokio::io::BufReader::new(file);
+}
+
+pub async fn scan_nmap(listener_ip: &str, output_file: &str, scan_type: ScanProtocol) {
     let mut nmap_args = vec![];
 
     let output_file = match scan_type {
-        Protocol::TCP => format!("{}_tcp", output_file),
-        Protocol::UDP => format!("{}_udp", output_file),
-        Protocol::BOTH => format!("{}_both", output_file),
+        ScanProtocol::TCP => format!("{}_tcp", output_file),
+        ScanProtocol::UDP => format!("{}_udp", output_file),
+        ScanProtocol::BOTH => format!("{}_both", output_file),
     };
 
     match scan_type {
-        Protocol::TCP => {
+        ScanProtocol::TCP => {
             nmap_args.extend(vec![
                 "-p",
                 "1-65535",
@@ -67,7 +120,7 @@ pub async fn scan_nmap(listener_ip: &str, output_file: &str, scan_type: Protocol
                 &output_file,
             ]);
         }
-        Protocol::UDP => {
+        ScanProtocol::UDP => {
             nmap_args.extend(vec![
                 "-p",
                 "1-65535",
@@ -79,7 +132,7 @@ pub async fn scan_nmap(listener_ip: &str, output_file: &str, scan_type: Protocol
                 &output_file,
             ]);
         }
-        Protocol::BOTH => {
+        ScanProtocol::BOTH => {
             nmap_args.extend(vec![
                 "-p",
                 "1-65535",
