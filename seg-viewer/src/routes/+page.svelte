e27adf453afc96bd8dac6bfd94028eb33672950b
Cory Sabol <corygrandhamsabol@gmail.com>
2024-11-21 12:53:28 -0800

    add example sigma.js graph

--- seg-viewer/src/routes/+page.svelte
+++ seg-viewer/src/routes/+page.svelte
@@ -4,6 +4,10 @@
   import { open as openFile } from "@tauri-apps/plugin-fs";
   import { appConfigDir } from "@tauri-apps/api/path";
 
+  import Sigma from "sigma";
+  import Graph from "graphology";
+  import { onMount } from "svelte";
+
   // Data ====
   type NodeDatum = {
     id: string;
@@ -62,18 +66,75 @@
       // user selected a single directory
     }
   };
+
+  const processDataToGraph = (data: GraphData) => {
+    // TODO: Process nodes
+    // TODO: Process and add edges
+    console.log("TODO: note yet implemented");
+  };
+
+  let container: HTMLElement;
+
+  const graph = $state(new Graph());
+  onMount(() => {
+    // Need to figure out how to make the graph a reactive value
+    // So that the graph can be updated with imported data by the user.
+    //const graph = new Graph();
+
+    graph.addNode("John", {
+      x: 0,
+      y: 10,
+      size: 15,
+      label: "John",
+      color: "blue",
+    });
+    graph.addNode("Mary", {
+      x: 10,
+      y: 0,
+      size: 10,
+      label: "Mary",
+      color: "green",
+    });
+    graph.addNode("Thomas", {
+      x: 7,
+      y: 9,
+      size: 20,
+      label: "Thomas",
+      color: "red",
+    });
+    graph.addNode("Hannah", {
+      x: -7,
+      y: -6,
+      size: 25,
+      label: "Hannah",
+      color: "teal",
+    });
+
+    graph.addEdge("John", "Mary");
+    graph.addEdge("John", "Thomas");
+    graph.addEdge("John", "Hannah");
+    graph.addEdge("Hannah", "Thomas");
+    graph.addEdge("Hannah", "Mary");
+
+    const renderer = new Sigma(graph, container);
+  });
 </script>
 
 <div class="container">
   <button onclick={handle_file_open}>Import data</button>
 </div>
 
-<div id="vis-container"></div>
+<div bind:this={container} id="vis-container" class="vis-container"></div>
 
 <style>
   .container {
     width: 100%;
     display: flex;
     flex-direction: column;
+  }
+
+  .vis-container {
+    width: 800px;
+    height: 600px;
   }
 </style>
